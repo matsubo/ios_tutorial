04335fccec38386af711c875f04096907cee7780
Yuki Matsukura <matsubokkuri@gmail.com>
2012-11-04 11:56:15 +0900

    Tutorial's done.

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -10,6 +10,12 @@
 
 @interface HelloWorldViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+- (IBAction)changeGreeting:(id)sender;
+
 @end
 
 @implementation HelloWorldViewController
@@ -20,10 +26,45 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)viewDidUnload
+{
+    [self setTextField:nil];
+    [self setLabel:nil];
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)changeGreeting:(id)sender {
+    
+    self.userName = self.textField.text;
+    
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.label.text = greeting;
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    if (theTextField == self.textField) {
+        [theTextField resignFirstResponder];
+    }
+    return YES;
+}
+
 
 @end
---
+++ README.md
@@ -0,0 +1,7 @@
+Sample application of Apple's tutorial
+============================================
+
+
+see:
+
+https://developer.apple.com/library/ios/#referencelibrary/GettingStarted/RoadMapiOS/chapters/RM_YourFirstApp_iOS/Articles/01_CreatingProject.html
